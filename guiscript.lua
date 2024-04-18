a9bbc2d1c4da53ae4e24f8764b0479e66c5d3d81
complexwaremain <166940834+complexwaremain@users.noreply.github.com>
2024-04-18 16:17:03 +0400

    Update guiscript.lua

--- guiscript.lua
+++ guiscript.lua
@@ -1,12 +1,12 @@
--- i think its me complex the mario
+-- Gui to Lua
+-- Version: 3.2
 
 -- Instances:
 
 local ScreenGui = Instance.new("ScreenGui")
-local GUIOpener = Instance.new("TextButton")
-local UICorner = Instance.new("UICorner")
-local UIGradient = Instance.new("UIGradient")
+local Fanware = Instance.new("TextButton")
 local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+local UICorner = Instance.new("UICorner")
 local Frame = Instance.new("Frame")
 local UICorner_2 = Instance.new("UICorner")
 local Close = Instance.new("TextButton")
@@ -14,45 +14,36 @@ local UIAspectRatioConstraint_2 = Instance.new("UIAspectRatioConstraint")
 local Frame_2 = Instance.new("Frame")
 local TextLabel = Instance.new("TextLabel")
 local TextLabel_2 = Instance.new("TextLabel")
-local TextButton = Instance.new("TextButton")
-local UICorner_3 = Instance.new("UICorner")
-local UIGradient_2 = Instance.new("UIGradient")
-local TextButton_2 = Instance.new("TextButton")
-local UICorner_4 = Instance.new("UICorner")
-local UIGradient_3 = Instance.new("UIGradient")
-local UIGradient_4 = Instance.new("UIGradient")
+local UIGradient = Instance.new("UIGradient")
 
 --Properties:
 
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-GUIOpener.Name = "GUI Opener"
-GUIOpener.Parent = ScreenGui
-GUIOpener.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-GUIOpener.BorderColor3 = Color3.fromRGB(0, 0, 0)
-GUIOpener.BorderSizePixel = 0
-GUIOpener.Position = UDim2.new(0.0584677421, 0, 0.441666722, 0)
-GUIOpener.Size = UDim2.new(0, 200, 0, 50)
-GUIOpener.Font = Enum.Font.FredokaOne
-GUIOpener.Text = "Complexware X Fanware"
-GUIOpener.TextColor3 = Color3.fromRGB(0, 0, 0)
-GUIOpener.TextSize = 16.000
-GUIOpener.TextStrokeTransparency = 2.000
+Fanware.Name = "Fanware"
+Fanware.Parent = ScreenGui
+Fanware.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Fanware.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Fanware.BorderSizePixel = 4
+Fanware.Position = UDim2.new(0.0584677421, 0, 0.441666722, 0)
+Fanware.Size = UDim2.new(0, 200, 0, 50)
+Fanware.Font = Enum.Font.FredokaOne
+Fanware.Text = "Fanware GUI Opener"
+Fanware.TextColor3 = Color3.fromRGB(0, 0, 0)
+Fanware.TextSize = 16.000
+Fanware.TextStrokeTransparency = 2.000
 
-UICorner.Parent = GUIOpener
-
-UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(146, 220, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(124, 187, 255))}
-UIGradient.Parent = GUIOpener
-
-UIAspectRatioConstraint.Parent = GUIOpener
+UIAspectRatioConstraint.Parent = Fanware
 UIAspectRatioConstraint.AspectRatio = 4.000
+
+UICorner.Parent = Fanware
 
 Frame.Parent = ScreenGui
 Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.418346763, 0, 0.358333319, 0)
+Frame.Position = UDim2.new(0.331653237, 0, 0.336666673, 0)
 Frame.Size = UDim2.new(0, 545, 0, 294)
 
 UICorner_2.CornerRadius = UDim.new(0.100000001, 0)
@@ -107,46 +98,14 @@ TextLabel_2.Text = "V1"
 TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel_2.TextSize = 34.000
 
-TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.BorderSizePixel = 0
-TextButton.Position = UDim2.new(0.117431194, 0, 0.302721083, 0)
-TextButton.Size = UDim2.new(0, 200, 0, 50)
-TextButton.Font = Enum.Font.Cartoon
-TextButton.Text = "Sprint"
-TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.TextSize = 39.000
-
-UICorner_3.Parent = TextButton
-
-UIGradient_2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(134, 162, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(51, 95, 255))}
-UIGradient_2.Parent = TextButton
-
-TextButton_2.Parent = Frame
-TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextButton_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_2.BorderSizePixel = 0
-TextButton_2.Position = UDim2.new(0.533944964, 0, 0.302721083, 0)
-TextButton_2.Size = UDim2.new(0, 200, 0, 50)
-TextButton_2.Font = Enum.Font.SourceSans
-TextButton_2.Text = "Fly"
-TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_2.TextSize = 43.000
-
-UICorner_4.Parent = TextButton_2
-
-UIGradient_3.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(124, 170, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(62, 91, 255))}
-UIGradient_3.Parent = TextButton_2
-
-UIGradient_4.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(24, 24, 24)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(48, 48, 48))}
-UIGradient_4.Rotation = 20
-UIGradient_4.Parent = Frame
+UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(24, 24, 24)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(48, 48, 48))}
+UIGradient.Rotation = 20
+UIGradient.Parent = Frame
 
 -- Scripts:
 
-local function PHTAUF_fake_script() -- GUIOpener.LocalScript 
-	local script = Instance.new('LocalScript', GUIOpener)
+local function GYHOT_fake_script() -- Fanware.LocalScript 
+	local script = Instance.new('LocalScript', Fanware)
 
 	local frame = script.Parent.Parent.Frame
 	
@@ -154,16 +113,16 @@ local function PHTAUF_fake_script() -- GUIOpener.LocalScript
 		frame.Visible = true
 	end)
 end
-coroutine.wrap(PHTAUF_fake_script)()
-local function IECXR_fake_script() -- GUIOpener.LocalScript 
-	local script = Instance.new('LocalScript', GUIOpener)
+coroutine.wrap(GYHOT_fake_script)()
+local function GQYAFSH_fake_script() -- Fanware.LocalScript 
+	local script = Instance.new('LocalScript', Fanware)
 
 	script.Parent.Active = true
 	script.Parent.Selectable = true
 	script.Parent.Draggable = true
 end
-coroutine.wrap(IECXR_fake_script)()
-local function YPKG_fake_script() -- Close.LocalScript 
+coroutine.wrap(GQYAFSH_fake_script)()
+local function FDKBUHV_fake_script() -- Close.LocalScript 
 	local script = Instance.new('LocalScript', Close)
 
 	local frame = script.Parent.Parent
@@ -172,75 +131,16 @@ local function YPKG_fake_script() -- Close.LocalScript
 		frame.Visible = false
 	end)
 end
-coroutine.wrap(YPKG_fake_script)()
-local function OFOL_fake_script() -- Frame.LocalScript 
+coroutine.wrap(FDKBUHV_fake_script)()
+local function XGXAAV_fake_script() -- Frame.LocalScript 
 	local script = Instance.new('LocalScript', Frame)
 
 	script.Parent.Active = true
 	script.Parent.Selectable = true
 	script.Parent.Draggable = true
 end
-coroutine.wrap(OFOL_fake_script)()
-local function YKLK_fake_script() -- TextButton.LocalScript 
-	local script = Instance.new('LocalScript', TextButton)
-
-	local button = script.Parent
-	local Player = game:GetService('Players')
-	local sprinting = false
-	
-	local function sprint()
-		local player = Player.LocalPlayer
-		if sprinting then
-			sprinting = false
-			button.Text = 'Sprint'
-			player.Character.Humanoid.WalkSpeed = 24
-		else
-			sprinting = true
-			button.Text = 'Sprint'
-			player.Character.Humanoid.WalkSpeed = 16
-		end
-	end
-	
-	button.MouseButton1Click:Connect(sprint)
-end
-coroutine.wrap(YKLK_fake_script)()
-local function HKULA_fake_script() -- TextButton_2.LocalScript 
-	local script = Instance.new('LocalScript', TextButton_2)
-
-	repeat wait() until game.Players.LocalPlayer.Character
-	
-	local player = game.Players.LocalPlayer
-	local character = player.Character
-	local humanoid = character:WaitForChild("Humanoid")
-	local torso = character:WaitForChild("LowerTorso")
-	local mouse = player:GetMouse()
-	
-	local enabled = false
-	
-	mouse.KeyDown:Connect(function(key)
-		if key "w" then
-			if enabled == false then
-				enabled true
-				local bodyvelocity = Instance.new("BodyVelocity",torso)
-				bodyvelocity.MaxForce = Vector3.new(math.huge,math.huge,math.huge
-					
-					while enabled == true do
-						bodyvelocity.Velocity = mouse.Hit.LookVector * 100
-						wait()
-					end
-					
-			end
-			
-			if enabled == true then
-				enabled = false
-				torso:FindFirstChildOfClass("BodyVelocity") : Destroy()
-			end
-			
-		end
-	end)
-end
-coroutine.wrap(HKULA_fake_script)()
-local function VNOHIOX_fake_script() -- Frame.LocalScript 
+coroutine.wrap(XGXAAV_fake_script)()
+local function YYUS_fake_script() -- Frame.LocalScript 
 	local script = Instance.new('LocalScript', Frame)
 
 	local UIgradient = script.Parent.UIStroke.UIGradient
@@ -250,4 +150,4 @@ local function VNOHIOX_fake_script() -- Frame.LocalScript
 		UIgradient.Rotation += 2
 	end)
 end
-coroutine.wrap(VNOHIOX_fake_script)()
+coroutine.wrap(YYUS_fake_script)()
